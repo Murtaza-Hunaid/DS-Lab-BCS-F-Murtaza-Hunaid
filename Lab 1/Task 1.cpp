d85a92eecab6990f00689d04ffcac2ef28aa0f8d
Murtaza-Hunaid <murtazainhouse@gmail.com>
2025-08-23 22:50:04 +0500

    Update Task 1.cpp

--- Lab 1/Task 1.cpp
+++ Lab 1/Task 1.cpp
@@ -1,1 +1,47 @@
-.
+#include <iostream>
+using namespace std;
+
+class BankAccount {
+private:
+    double bal;
+
+public:
+    BankAccount() {
+        bal = 0.0;
+    }
+
+    BankAccount(double initialBal) {
+        bal = initialBal;
+    }
+
+    BankAccount(const BankAccount& obj) {
+        bal = obj.bal;
+    }
+
+    void withdraw(double amount) {
+        if (amount <= bal) {
+            bal -= amount; } 
+        else {
+            cout << "Insufficient balance" << endl; }
+    }
+
+    void displayBalance() {
+        cout << "Balance: Rs. " << bal << endl;
+    }
+};
+
+int main() {
+    BankAccount account1;
+    cout << "Account1 "; 
+    account1.displayBalance();
+    BankAccount account2(1000.0);
+    cout << "Account2 ";
+    account2.displayBalance();
+    BankAccount account3(account2);
+    account3.withdraw(200);
+    cout << "Account3 ";
+    account3.displayBalance();
+    cout << "Account2 ";
+    account2.displayBalance();
+    return 0;
+}
