9cedbb68b7c11b068a874648b82a06c8894e2371
Murtaza-Hunaid <murtazainhouse@gmail.com>
2025-09-13 18:39:05 +0500

    Create Task 2.cpp

---
+++ Lab 4/Task 2.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+void bubbleSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        bool swapped = false;
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);
+                swapped = true; } }
+        if (!swapped) {
+            break; } }
+}
+
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " "; }
+    cout << endl;
+}
+
+int main() {
+    int n;
+    cout << "Enter the number of elements: ";
+    cin >> n;
+    int arr[n];
+    cout << "Enter " << n << " integers: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i]; }
+    cout << "Unsorted array: ";
+    printArray(arr, n);
+    bubbleSort(arr, n);
+    cout << "Sorted array: ";
+    printArray(arr, n);
+    return 0;
+}
